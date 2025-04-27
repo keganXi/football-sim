1818b23be6eab7bf0fb9cf95e8dc9efc174a965f
Kegan Overberg <=>
2025-04-27 22:16:05 +0200

    create dfs algorithms scanning passing lanes

---
+++ game.py
@@ -0,0 +1,65 @@
+# Grid (Pitch)
+#
+#   0   1   2   3   4   5   6   7   8 (-1)
+#   A   B   C   D   E   F   G   H   I
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9 (-1)
+#
+# players on the pitch should be 1 or -1, the whole pitch should be 0's, and and player position
+# should correspond to pitch coordinates, that how we retrieve the player
+
+import numpy as np
+import time
+from passing import POSITION_COORDINATES
+from players import PLAYERS
+from scan import scan_pitch
+
+
+COLS, ROWS = 9, 10
+
+PITCH = np.zeros((ROWS, 9))
+PITCH = PITCH.astype(int)
+
+for key, value in POSITION_COORDINATES.items():
+    row = value[0]
+    col = value[1]
+    print(f"Position {key}: {PITCH[row][col]}")
+    PITCH[row][col] = 1
+
+
+print(PITCH)
+print(f"rows: {len(PITCH)}")
+print(f"columns: {len(PITCH[0])}")
+print(f"Position: {PITCH[0][4]}") # (-1, 5) -> (row, col)
+print("\nScan for passing lanes...")
+
+MINUTE = 0
+FULL_TIME = 90
+
+player_position = POSITION_COORDINATES["ST"]
+print(player_position)
+
+while MINUTE < FULL_TIME:
+    MINUTE+=1
+
+    player = scan_pitch(PITCH, player_position)
+
+    get_position = ""
+    for key, value in POSITION_COORDINATES.items():
+        if value == player:
+            get_position = key
+
+    position = ()
+    for item in PLAYERS:
+        if item["role"] == get_position:
+            print(f"{item["name"]} - {item["role"]}")
+    player_position = player
+    time.sleep(1)
---
+++ scan.py
@@ -0,0 +1,23 @@
+def scan_pitch(grid, position, visited=None):
+    # NOTE: Depth-first search
+    if visited is None:
+        visited = set()
+
+    visited.add(position)
+
+    # Check adjacent positions (up, down, left, right)
+    row, col = position
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+
+    for dr, dc in directions:
+        new_row, new_col = row + dr, col + dc
+
+        # Ensure the new position is within bounds and not already visited
+        if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and (new_row, new_col) not in visited:
+            if grid[new_row, new_col] != 0:  # Check if it's occupied (player present)
+                print(f"Found player at ({new_row}, {new_col})")
+                return (new_row, new_col)
+            else:
+                result = scan_pitch(grid, (new_row, new_col), visited)
+                if result is not None:
+                    return result
